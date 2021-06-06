fe3243508d9e2f6b00bdbeaa1ed15ce85b903ca7
Vlad Dmitrievich <me@2tunnels.com>
2021-06-06 22:32:32 +0200

    Tweak

--- service/apps/core/models.py
+++ service/apps/core/models.py
@@ -14,21 +14,15 @@ class PublicDocument(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     document = models.FileField()
     uploader = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now=True)
 
 
 class PrivateDocument(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     document = PrivateDocumentField()
     uploader = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now=True)
 
 
 class PrivatePhoto(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     photo = PrivatePhotoField()
     uploader = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now=True)
